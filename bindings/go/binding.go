36ed0c0ea31a309481ee67d48fa564713e052a81
Viruthagiri Thirumavalavan <giridharanbtech@gmail.com>
2026-03-01 11:25:38 +0530

    feat: go bindings

--- bindings/go/binding.go
+++ bindings/go/binding.go
@@ -1,9 +1,9 @@
 package tree_sitter_vue
 
-// #cgo CFLAGS: -std=c11 -fPIC -I../../src
+// #cgo CPPFLAGS: -I../../src
+// #cgo CFLAGS: -std=c11 -fPIC
 // #cgo LDFLAGS: -lstdc++
 // #include "../../src/parser.c"
-// #include "../../src/scanner.cc"
 import "C"
 
 import "unsafe"
